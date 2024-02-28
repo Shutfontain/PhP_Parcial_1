e7beb71229d88bd8a9a5ed43536ed72df7f62ffd
Shutfontain <159972602+Shutfontain@users.noreply.github.com>
2024-02-28 10:48:11 -0600

    Update Inicio.php

--- Tarea 1.4/Inicio.php
+++ Tarea 1.4/Inicio.php
@@ -1,1 +1,29 @@
-Profe no se Guardo el codigo por algun error pero ya lo voy a subir 
+<html>
+    <head>
+        <link rel="stylesheet" href="Estilos2.css">
+    </head>
+    <body>
+       <h1 class="Barrr">OmegaUp</h1>
+
+       <div class="HHM"><!---->
+
+       <h2>Inicio</h2>
+
+       <p>Tarea 1.4: problemas con condicionales</p>
+       <p>Entregar una página que 
+           incluya los tres problemas 
+           adjuntos.
+        </p>
+       <br>
+       <u>
+        <li><a href="Inicio.php">Inicio</a></li>
+        <li><a href="Cuántos valen 7.php">Cuántos valen 7</a></li>
+        <li><a href="Estaciones del Año.php">Estaciones del año</a></li>
+        <li><a href="El mayor de tres números.php">El mayor de tres números</a></li>
+       </u>
+        <p>Alfredo Sánchez Oseguera 4to de programación
+        </p>
+        </div>
+        
+    </body><!---->
+</html>
