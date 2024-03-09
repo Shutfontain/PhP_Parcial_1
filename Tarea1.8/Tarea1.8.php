71085c8872a1a5438451719ce35b9c476252d586
Shutfontain <159972602+Shutfontain@users.noreply.github.com>
2024-03-09 08:12:05 -0600

    Create Tarea1.8.php

---
+++ Tarea1.8/Tarea1.8.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+    <link rel="stylesheet" href="Estilos.css">
+</head>
+<body>
+    <div class="Barrr">
+        <br>
+   <h1 class="Barrr">OmegaUp</h1><br>
+   </div>
+   
+
+   <div class="HHM"><!---->
+
+   <h2>Inicio</h2>
+
+   <p>Tarea 1.4: problemas con condicionales</p>
+   <br>
+   <ul>
+    <li><a href="Autores.php">Autores</a></li>
+    <li><a href="Bit.php">Bit de paridad</a></li>
+    <li><a href="Adquisición.php">Adquisición de Letras</a></li>
+   </ul>
+    </p>
+    </div>
+
+</body><!---->
+</html>
