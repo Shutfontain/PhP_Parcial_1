d8982b20b3b000f5cd50375794a2c01a178bd3be
Shutfontain <159972602+Shutfontain@users.noreply.github.com>
2024-05-22 22:02:45 -0600

    Add files via upload

---
+++ Tarea2.2/jijija.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <h1>Funciones con PHP</h1>
+    <div class="container">
+        <div class="form-container">
+            <h3>11823. Evaluando Una Función Rara</h3>
+            <form action="hhm1.php" method="post">
+                <input type="text" name="x" placeholder="X">
+                <input type="submit" value="Enviar">
+            </form>
+        </div>
+        <hr>
+        <div class="form-container">
+            <h3>1037. Juego de letras</h3>
+            <form action="hhm2.php" method="post">
+            <input type="text" name="p" placeholder="Cadena">
+            <input type="submit" value="Enviar">
+        </form>
+        </div>
+    </div>
+</body>
+</html>
